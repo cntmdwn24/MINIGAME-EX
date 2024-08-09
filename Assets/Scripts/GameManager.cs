1b6d141f2ae1adf2a2d3622afd95c7fdadbcc2a6
cntmdwn24 <162384860+cntmdwn24@users.noreply.github.com>
2024-08-09 19:58:23 +0900

    2024.08.09-1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,11 +12,21 @@ public class GameManager : MonoBehaviour
     public TMP_Text keyText;
     public TMP_Text coinText;
 
-    public GameObject successMessagePrefab; // 성공 메시지 프리팹
-    public GameObject failureMessagePrefab; // 실패 메시지 프리팹
-    public Transform messageParent; // 메시지 프리팹을 소환할 부모 객체 (예: Canvas의 자식)
+    public GameObject successMessagePrefab;
+    public GameObject failureMessagePrefab;
+    public GameObject lockedStageMessagePrefab; // "구매가 필요합니다" 메시지 프리팹
+    public GameObject stageSelectedMessagePrefab; // "선택됨" 메시지 프리팹
+    public GameObject SelectGamePanel;
+    public GameObject MenuPanel;
+    public GameObject StuffPanel;
+    public Transform messageParent;
+
+    public GameObject[] miniGamePrefabs;
+    public Transform gameSpawnPoint;
 
     private const int STAGE_COST = 10;
+    private int selectedStageIndex = -1;
+    private GameObject currentMiniGameInstance;
 
     void Awake()
     {
@@ -37,62 +47,85 @@ public class GameManager : MonoBehaviour
         UpdateUI();
     }
 
-    public void GameOver()
+    public void GameStart()
     {
-        gameOverPanel.SetActive(true);
-        Time.timeScale = 0;
+        MenuPanel.SetActive(true);
+        StuffPanel.SetActive(true);
+    }
+
+    public void SelectGame()
+    {
+        SelectGamePanel.SetActive(true);
+    }
+
+    public void CloseSelect()
+    {
+        SelectGamePanel.SetActive(false);
+    }
+
+    public void OnStageButtonClick(int stageIndex)
+    {
+        if (gameData.Games[stageIndex])
+        {
+            selectedStageIndex = stageIndex;
+            ShowMessage(stageSelectedMessagePrefab); // "스테이지 선택됨" 메시지
+            CloseSelectGamePanel(); // 선택했을 때만 패널을 닫음
+        }
+        else
+        {
+            ShowMessage(lockedStageMessagePrefab); // "잘못된 스테이지 인덱스" 메시지
+        }
+    }
+
+    public void UnlockStage(int stageIndex)
+    {
+        if (stageIndex >= 0 && stageIndex < gameData.Games.Length)
+        {
+            if (!gameData.Games[stageIndex])
+            {
+                gameData.Key -= STAGE_COST;
+                UpdateUI();
+                gameData.Games[stageIndex] = true;
+                ShowMessage(successMessagePrefab); // "스테이지 잠금 해제 성공" 메시지
+            }
+        }
+        else
+        {
+            ShowMessage(failureMessagePrefab); // "잘못된 스테이지 인덱스" 메시지
+        }
+    }
+
+    public void PlaySelectedStage()
+    {
+        if (selectedStageIndex >= 0 && selectedStageIndex < miniGamePrefabs.Length)
+        {
+            if (gameData.Games[selectedStageIndex])
+            {
+                if (currentMiniGameInstance != null)
+                {
+                    Destroy(currentMiniGameInstance);
+                }
+
+                currentMiniGameInstance = Instantiate(miniGamePrefabs[selectedStageIndex], gameSpawnPoint.position, Quaternion.identity);
+                MenuPanel.SetActive(false);
+            }
+            else
+            {
+                ShowMessage(lockedStageMessagePrefab); // "구매가 필요합니다" 메시지
+            }
+        }
+        else
+        {
+            ShowMessage(stageSelectedMessagePrefab); // "선택됨" 메시지
+            SelectGamePanel.SetActive(true);
+            MenuPanel.SetActive(false);
+        }
     }
 
     public void RestartGame()
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-
-    public void OnStageButtonClick(int stageIndex)
-    {
-        if (gameData.Key >= STAGE_COST)
-        {
-            gameData.Key -= STAGE_COST;
-            UnlockStage(stageIndex);
-            ShowMessage(successMessagePrefab); // 성공 메시지 표시
-            UpdateUI();
-        }
-        else
-        {
-            ShowMessage(failureMessagePrefab); // 실패 메시지 표시
-        }
-    }
-
-    void UnlockStage(int stageIndex)
-    {
-        if (stageIndex >= 0 && stageIndex < gameData.Games.Length)
-        {
-            gameData.Games[stageIndex] = true;
-            Debug.Log("Stage " + stageIndex + " unlocked!");
-        }
-    }
-
-    void ShowMessage(GameObject messagePrefab)
-    {
-        if (messagePrefab != null && messageParent != null)
-        {
-            GameObject messageInstance = Instantiate(messagePrefab, messageParent);
-            Destroy(messageInstance, 2f); // 2초 후 메시지 제거
-        }
-    }
-
-    void UpdateUI()
-    {
-        if (keyText != null)
-        {
-            keyText.text = "Keys: " + gameData.Key.ToString();
-        }
-
-        if (coinText != null)
-        {
-            coinText.text = "Coins: " + gameData.Coin.ToString();
-        }
     }
 
     [ContextMenu("To Json Data")]
@@ -113,6 +146,34 @@ public class GameManager : MonoBehaviour
             gameData = JsonUtility.FromJson<GameData>(jsonData);
             UpdateUI();
         }
+    }
+
+    void ShowMessage(GameObject messagePrefab)
+    {
+        if (messagePrefab != null && messageParent != null)
+        {
+            GameObject messageInstance = Instantiate(messagePrefab, messageParent);
+            Destroy(messageInstance, 2f);
+        }
+    }
+
+    void UpdateUI()
+    {
+        if (keyText != null)
+        {
+            keyText.text = "Keys: " + gameData.Key.ToString();
+        }
+
+        if (coinText != null)
+        {
+            coinText.text = "Coins: " + gameData.Coin.ToString();
+        }
+    }
+
+    void CloseSelectGamePanel()
+    {
+        SelectGamePanel.SetActive(false);
+        MenuPanel.SetActive(true);
     }
 
     [System.Serializable]
