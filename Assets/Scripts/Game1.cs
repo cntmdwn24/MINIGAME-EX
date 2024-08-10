2bbf3feef21fcf8df529e247f9a026969c559a3d
cntmdwn24 <162384860+cntmdwn24@users.noreply.github.com>
2024-08-10 15:58:01 +0900

    2024.08.10

--- Assets/Scripts/Game1.cs
+++ Assets/Scripts/Game1.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Game1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text Score;
+    public TMP_Text Bear1;
+    public TMP_Text Bear2;
+    public TMP_Text Bear3;
+    public Button Pause;
+
+    private int score = 0;
+    private bool isPaused = false;
+
     void Start()
+    {
+        Pause.onClick.AddListener(TogglePause);
+
+        UpdateScore();
+        UpdateBearStatus();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateScore()
     {
-        
+        Score.text = "Score: " + score;
+    }
+    
+    void UpdateBearStatus()
+    {
+        Bear1.text = "Bear 1: Active";
+        Bear2.text = "Bear 2: Active";
+        Bear3.text = "Bear 3: Active";
+    }
+
+    void TogglePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+        Pause.GetComponentInChildren<TMP_Text>().text = isPaused ? "Resume" : "Pause";
     }
 }
